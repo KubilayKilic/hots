e1ea9b2d1573b8d8600125fa75fea389d83b02a8
KubilayKilic <kilickubilaykaan@gmail.com>
2025-05-08 18:21:53 +0300

    card style changed and rating system added

--- app/filmler/page.tsx
+++ app/filmler/page.tsx
@@ -8,11 +8,13 @@ const page = () => {
         title="Kara Şövalye"
         description="When a menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman, James Gordon and Harvey Dent must work together to put an end to the madness."
         imageUrl="/batman-cover.jpg"
+        resonance={1}
       />
       <Card
         title="Örümcek Adam: Örümcek Evreninde"
         description="Teen Miles Morales becomes the Spider-Man of his universe and must join with five spider-powered individuals from other dimensions to stop a threat for all realities."
         imageUrl="/spi-cover.jpg"
+        resonance={2}
       />
     </div>
   );
--- componenets/ui/Card.tsx
+++ componenets/ui/Card.tsx
@@ -6,9 +6,15 @@ interface CardProps {
   title: string;
   description: string;
   imageUrl?: string;
+  resonance: number; // 1, 2 veya 3 yıldız skoruna göre emoji eklenecek
 }
 
-const Card: React.FC<CardProps> = ({ title, description, imageUrl }) => {
+const Card: React.FC<CardProps> = ({
+  title,
+  description,
+  imageUrl,
+  resonance,
+}) => {
   return (
     <div className="relative w-[220px] h-[330px] rounded-lg shadow-lg overflow-hidden group">
       <img
@@ -17,8 +23,11 @@ const Card: React.FC<CardProps> = ({ title, description, imageUrl }) => {
         className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
       />
       <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center text-center opacity-0 group-hover:opacity-100 transition-opacity duration-400 px-4">
-        <h3 className="text-white text-xl font-bold mb-2">{title}</h3>
-        <p className="text-white text-sm">{description}</p>
+        <h3 className="text-white text-xl font-bold mt-4">{title}</h3>
+        <p className="text-white text-sm mt-2">{description}</p>
+        <div className="text-yellow-400 text-lg flex justify-between mt-auto mb-4">
+          {"⭐".repeat(resonance)}
+        </div>
       </div>
     </div>
   );
