80d5311894be38e1558300adfe4583acc463918e
KubilayKilic <kilickubilaykaan@gmail.com>
2025-07-01 12:08:39 +0300

    dedicated button for adding content rather than manually adding from database.

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ import KitaplarButon from "@/componenets/Butonlar/KitaplarButon";
 import DizilerButon from "@/componenets/Butonlar/DizilerButon";
 import OyunlarButon from "@/componenets/Butonlar/OyunlarButon";
 import MuziklerButon from "@/componenets/Butonlar/MüziklerButon";
+import EkleButon from "@/componenets/Butonlar/EkleButon";
 
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center gap-4">
-      <h1 className="text-3xl text-amber-50 font-bold mb-6">🎯 Arşiv</h1>
+      <h1 className="text-3xl text-amber-50 font-bold mb-6">🗄 Arşiv</h1>
       <div className="flex flex-wrap justify-center gap-4">
         <FilmlerButon />
         <DizilerButon />
@@ -15,6 +16,7 @@ export default function Home() {
         <KitaplarButon />
         <MuziklerButon />
       </div>
+      <EkleButon />
     </main>
   );
 }
